840188d30408ae7dc37f9826e90f04b40269aaea
Haley Fogelson <fogelsonhaley@gmail.com>
2022-10-13 17:49:24 -0400

    Recent updates to translate

--- BestPath.py
+++ BestPath.py
@@ -4,134 +4,130 @@
 #include <stdio.h>
 #include <string.h>
 
-import time
 import csv
+import RPi.GPIO as GPIO
+from time import sleep
 
-const int stepsPerRevolution = 20
-const int servoPin = 6
+# Pins for Motor Driver Inputs 
+Motor1A = 22
+Motor1B = 27
+Motor1E = 17
+
+Motor2A = 4;
+Motor2B = 23;
+Motor2C = 25;
+
+#STEPSPERREV = 20
+#servoPin = 6
 
 #Hardware Initialization
-Servo penServo#initiates servo
-Stepper xAxis(stepsPerRevolution, 2, 3, 10, 11) #pins x axis motor are connected to
-Stepper yAxis(stepsPerRevolution, 4, 5, 8, 9) #pins y axis motor are connected to
+#Servo penServo#initiates servo
+#Stepper xAxis(STEPSPERREV, 2, 3, 10, 11) #pins x axis motor are connected to
+#Stepper yAxis(STEPSPERREV, 4, 5, 8, 9) #pins y axis motor are connected to
 
 #Dynamic Memory Initialization
-bool drawing = true
-int xStep
-int yStep
-int xOld
-int yOld
-int coordX
-int coordY
+drawing = true
+xStep = 0
+yStep = 0
+xOld = 0
+yOld = 0
+coordX = 0
+coordY = 0
 
 #Function Declarations
-void movePenUp()
-void movePenDown()
-void drawX(int n)
-void drawY(int n)
-int blockingRead()
-int convertSerialInputStringToInt()
+# def movePenUp()
+# def movePenDown()
+# def drawX(n)
+# def drawY(n)
+# def blockingRead()
+# def convertSerialInputStringToInt()
 
-void setup()
-{
-  xAxis.setSpeed(100)
-  yAxis.setSpeed(100)
-  penServo.attach(servoPin)
-  Serial.begin(9600)
-}
+def setup():
+  GPIO.setmode(GPIO.BCM)        # GPIO Numbering
+  GPIO.setup(Motor1A,GPIO.OUT)  # All pins as Outputs
+  GPIO.setup(Motor1B,GPIO.OUT)
+  GPIO.setup(Motor1E,GPIO.OUT)
+
 
 def loop():
     drawing = True
     while drawing:
         time.sleep(6)
-        int numCoords = blockingRead() #reads total number of coordinates
+        numCoords = blockingRead() #reads total number of coordinates
     
         movePenUp() #initializes pen as up
         xOld = 0 #initializes, axis start at (0.0)
         yOld = 0
 
         for i in numCoords:
-        print("\n")
-        print("@GetNext") #gives order to imageProcessing.py
-        sleep(3000)
+          print("\n")
+          print("@GetNext") #gives order to imageProcessing.py
+          sleep(3000)
 
-        #python sends in next two coords
-        coordX = blockingRead() #reads x
-        coordY = blockingRead() #reads y
+          #python sends in next two coords
+          coordX = blockingRead() #reads x
+          coordY = blockingRead() #reads y
 
-        #calculate difference
-        xStep = coordX - xOld
-        yStep = coordY - yOld
+          #calculate difference
+          xStep = coordX - xOld
+          yStep = coordY - yOld
 
-        if ((abs(xStep) > 5) or (abs(yStep) > 5)) #pixels too far apart, lift up pen and move
-        {
+          if ((abs(xStep) > 5) or (abs(yStep) > 5)): #pixels too far apart, lift up pen and move
             #movePenUp()
-            drawX((int)xStep)
-            drawY((int)yStep)
-            #movePenDown()
-        }
-        else #draws
-        {
-            drawX((int)xStep)
-            drawY((int)yStep)
-        }
-        xOld = coordX
-        yOld = coordY
+              drawX(xStep)
+              drawY(yStep)
+              #movePenDown()
+          
+          else: #draws
+            drawX(xStep)
+            drawY(yStep)
+
+          xOld = coordX
+          yOld = coordY
+
         #movePenUp()
     drawing = false #stop drawing when image is complete
 
-"""
-void movePenUp() #turn off LED
-{
-  penServo.write(70)#pulls pen up
-  delay(1000) #pause for 3 seconds
-}
+#Here we have all the helper functions
 
-void movePenDown() #turn on LED
-{
-  penServo.write(0)#pulls pen up
-  delay(1000) #pause for 3 seconds
-}
-"""
+def movePenUp(): #turn off LED
+  #penServo.write(70)#pulls pen up
+  print("moving pen up")
+  sleep(1000) #pause for 3 seconds
 
-"""
-void drawX(int stepsToMove)
-{
-  int steps
-  if (stepsToMove > 0) #to move forwards
-  {
+def movePenDown(): #turn on LED
+  #penServo.write(0)#pulls pen up
+  print("moving pen down")
+  sleep(1000) #pause for 3 seconds
+
+
+
+#function for drawing x
+def drawX(stepsToMove):
+  steps = 0
+  if (stepsToMove > 0): #to move forwards
     steps = 1
-  }
-  else #to move backwards
-  {
+  else: #to move backwards
     stepsToMove *= -1
     steps = -1
-  }
-  for (int i = 0 i < (int)stepsToMove i++)
-  {
-    xAxis.step(steps)
-    delay(10)
-  }
-}
+  for x in range(stepsToMove):
+    #xAxis.step(steps)
+    sleep(10)
 
-void drawY(int stepsToMove)
-{
-  int steps
-  if (stepsToMove > 0) #to move forwards
-  {
+
+#function for drawing y
+def drawY(stepsToMove):
+  print("drawing y direction")
+  steps = 0
+  if (stepsToMove > 0): #to move forwards
     steps = 1
-  }
-  else #to move backwards
-  {
+  else: #to move backwards
     stepsToMove *= -1
     steps = -1
-  }
-  for (int i = 0 i < (int)stepsToMove i++)
-  {
+
+  for x in range(stepsToMove):
     yAxis.step(steps)
-    delay(10)
-  }
-}
+    sleep(10)
 
 """
 def blockingRead(): #buffers before reading to make sure serial input is fully sent
@@ -168,7 +164,7 @@ def convertSerialInputStringToInt(): #convert csv to int#
   }
 
   #parses that string into an int
-  int len = inputString.length()
+  len = inputString.length()
   for i in len:
     if (inputString[i] == '.') #python sends string with number that looks like a float, 
     {                         #end parsing when reaching the decimal because we just want the integer part
@@ -181,3 +177,5 @@ def convertSerialInputStringToInt(): #convert csv to int#
   resultInt = resultStr.toInt()
 
   return resultInt
+
+  """
